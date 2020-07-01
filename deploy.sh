c5e5c29b50f089893aaffcebd2b404990f58cea2
Bryan Deagle <bryan.deagle@ni.com>
2020-07-01 12:57:33 -0500

    network

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 docker rm -f huginn_agents
 docker build -t deagle/agents:stable .
 
-docker run --init -d \
+docker run --init \
     --name huginn_agents \
+    --network="huginn-net"
     --restart always \
-    -p 5800:5000 \
+    -p 80:5000 \
     deagle/agents:stable
