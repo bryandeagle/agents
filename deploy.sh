00fe3b6d1673c6839502ea2a49083609bb9731d9
Bryan Deagle <bryan.deagle@ni.com>
2020-07-01 14:23:37 -0500

    expenses

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 docker rm -f huginn_agents
 docker build -t deagle/agents:stable .
 
-docker run -d \
+docker run -it \
     --name huginn_agents \
     --network=huginn-net \
     --restart always \
